3eccda83f6b95451a2b97722254d06ee886535f6
陳珏安 <gian@chenjueandeAir.Home>
2020-09-13 14:04:02 +0800

    add muti_position

--- full_text_search/urls.py
+++ full_text_search/urls.py
@@ -4,5 +4,6 @@ from full_text_search import views
 
 urlpatterns = [
     path('keyword_search/',views.keyword),
-    path('show_content/',views.content)
+    path('show_content/',views.content),
+    path('muti_position/',views.muti_pos)
 ]
--- full_text_search/views.py
+++ full_text_search/views.py
@@ -24,13 +24,13 @@ def keyword(request):
         #     article_list.append(data)
         cursor = connection.cursor()
         cursor.execute("""
-        SELECT a.word,a.position,b.name,b.id 
+        SELECT a.word,a.position,b.name,b.id,a.id
         FROM full_text_search_inverted_index as a 
         INNER JOIN full_text_search_article as b 
         WHERE a.word LIKE '%s' AND a.article_id = b.id 
         ORDER BY a.word ASC """%(keyword+'%'))
         results = cursor.fetchall()
-        column_name = ['word','position','title','article_id']
+        column_name = ['word','position','title','article_id','key']
         result_data=[]
         for result in results:
             result_data.append(dict(zip(column_name,result)))
@@ -50,4 +50,56 @@ def content(request):
         return JsonResponse(result)
     else :
         return HttpResponse("please use GET")
+
+
+def muti_pos(request) :
+    if request.method == "GET":
+        try:
+            keyword = request.GET['keyword']
+        except Exception as e:
+            print(e)
+            return HttpResponse("should include keyword")
+        cursor = connection.cursor()
+        cursor.execute("""
+        SELECT a.word,a.position,b.name,b.id ,a.id
+        FROM full_text_search_inverted_index as a 
+        INNER JOIN full_text_search_article as b 
+        WHERE a.word LIKE '%s' AND a.article_id = b.id 
+        ORDER BY a.word ASC """%(keyword+'%'))
+        results = cursor.fetchall()
+
+        result_data=[]
+        article_dic ={} #{title : {word : position }}  #相同title word 的資料放一起
+        for result in results: 
+            article_key = result[2]  
+            word_key = result[0]
+            if article_key in article_dic :
+                article_value = article_dic[article_key]
+                if word_key in article_value :
+                    article_value[word_key] = str(article_value[word_key]) +"," +str(result[1])
+                else :
+                    article_value[word_key] = result[1]
+
+            else :
+                article_dic[article_key] = {}
+                article_dic[article_key][word_key] = result[1]
+
+        key = 1
+        for t in article_dic.items():
+            print("key:")
+            print(t[0])
+            for v in t[1].items():
+                data = {
+                    "word" : v[0],
+                    "position" : v[1],
+                    "title" : t[0],
+                    "key" : key,
+                }
+                key += 1
+                result_data.append(data)
+        return JsonResponse(result_data , safe=False)
+    else :
+        return HttpResponse("please use GET")
+    
+
 
