1180ba9148684f2d3da0c1511232e44a69f67f6a
陳珏安 <gian@chenjueandeAir.Home>
2020-09-13 17:57:06 +0800

    antd frontend

--- frontend-fts/src/page/home.js
+++ frontend-fts/src/page/home.js
@@ -1,32 +1,71 @@
-import React, { useState, useEffect } from 'react';
-import '@coreui/coreui/dist/css/coreui.min.css'
+import React, { useState, useEffect, useReducer } from 'react';
 import './home.css';
+import { Table } from 'antd';
 import HomeService from './home.service';
+import 'antd/dist/antd.css';
+import Search from 'antd/lib/input/Search';
 
 export const Home = () => {
     const service = HomeService();
-    const [data, setData] = useState([]);
-    const [text, setText] = useState('');
+    const [data, setData] = useState([]); //[] 初始為空array
+    const [ignore, forceUpdate] = useReducer(x => x + 1, 0); //畫面更新 useState 的變數沒改變時畫面不會更新
+    const column = [
+        { title: 'Word', dataIndex: 'word', key: 'word' },
+        { title: 'Position', dataIndex: 'position', key: 'position' },
+        { title: 'Title', dataIndex: 'title', key: 'title' }
+    ]
+    const expandable = {
+        expandedRowRender: record => <div
+            className='table-detail'
+            dangerouslySetInnerHTML={{ __html: record.article }}
+        ></div>,
+        expandRowByClick: true
+    }
+    const expandRow = async (record) => {
+        let article = await service.getArticleById(record.article_id);
+        let index = record.position.split(',');
+        let contents = article.content.split(' ');
+        for (let i of index) {
+            contents[parseInt(i) - 1] = '<yellow-block>' + record.word + '</yellow-block>';
+        }
+        contents = contents.join(' ');
+        record.article = contents;
+        forceUpdate();
+    }
+    // setData(
+    //     {
+    //         ...data,
+    //         n : 
+    //     }
+    // )
 
     useEffect(() => {
         console.log(data);
     }, [data])
 
-    const searchFullText = async () => {
+    const searchFullText = async (text) => {
         setData(await service.search(text));
-    }
+    }  //parameter async( par )
 
     return (
         <div className="main-layout">
-            <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="input text you want to search" aria-describedby="button-addon2"
-                    onChange={(e) => { setText(e.target.value); }}
+            <Search
+                enterButton
+                onSearch={value => searchFullText(value)}
+                placeholder="type keyword"
+                style={{ margin: "20px 0px" }}
+            />
+            <div className="table-style">
+                <Table
+                    columns={column}
+                    rowKey={data => data.key}
+                    dataSource={data}
+                    onExpand={(expand, record) => {
+                        if (expand)
+                            expandRow(record);
+                    }}
+                    expandable={expandable}
                 />
-                <div className="input-group-append">
-                    <button className="btn btn-outline-dark" type="button" id="button-addon2"
-                        onClick={(e) => { searchFullText(); }}
-                    >Search</button>
-                </div>
             </div>
         </div>
     )
--- frontend-fts/src/page/home.service.js
+++ frontend-fts/src/page/home.service.js
@@ -2,13 +2,20 @@ import axios from 'axios';
 
 const HomeService = () => {
     const search = async (data) => {
-        
-        return (await axios.get('/full_text_search/keyword_search/?keyword=' + data)).data;;
+
+        let d = (await axios.get('/full_text_search/muti_position/?keyword=' + data)).data;
+        return d;
+    }
+
+    const getArticleById = async (id) => {
+        let d = (await axios.get('/full_text_search/show_content/?id=' + id)).data;
+        return d;
     }
 
 
     return {
-        search: search
+        search: search,
+        getArticleById: getArticleById
     }
 }
 
